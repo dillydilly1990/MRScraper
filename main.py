80f27aff600b7ef60877ed73893678c956d9cda8
deirdre behan <dee.behan@gmail.com>
2024-11-24 13:48:44 +0000

    1.1 Commit

--- main.py
+++ main.py
@@ -18,10 +18,13 @@ class DownloaderApp:
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
         self.console_url = tk.StringVar()
+        self.advanced_mode = tk.BooleanVar(value=False)
+        self.custom_url = tk.StringVar()
         self.do_region_limit = tk.BooleanVar(value=True)
         self.region = tk.StringVar(value="Europe")
         self.status = tk.StringVar(value="Ready")
         self.do_smart_limit = tk.BooleanVar(value=True)
+        self.skip_duplicates = tk.BooleanVar(value=False)
         self.output_dir = tk.StringVar(value="C:/")
 
         # Flags
@@ -30,6 +33,10 @@ class DownloaderApp:
         self.pause_flag = threading.Event()
 
         # Widgets
+        self.system_selector = None
+        self.system_text = None
+        self.url_entry_label = None
+        self.url_entry_box = None
         self.region_label = None
         self.region_selector = None
         self.limit_label = None
@@ -47,25 +54,39 @@ class DownloaderApp:
         frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
         # System selection section
-        system_frame = ttk.LabelFrame(frame, text="Directory", padding="10")
+        system_frame = ttk.LabelFrame(frame, text="Console", padding="10")
         system_frame.grid(row=0, column=0, columnspan=3, sticky=(tk.W, tk.E))
 
-        
+        self.system_text = (ttk.Label(system_frame, text="System:", width=20))
+        self.system_text.grid(row=0, column=0, sticky=tk.W)
 
-        ttk.Label(system_frame, text="System:", width=20).grid(row=0, column=0, sticky=tk.W)
         dir_path = os.path.dirname(os.path.realpath(__file__))
         file_path = os.path.join(dir_path, "systemlist.txt")
         with open(file_path, 'r') as f:
             consoles = [line.strip() for line in f.readlines()]
-        dropdown = ttk.Combobox(system_frame, textvariable=self.console_url, values=consoles, width=50)
-        dropdown.grid(row=0, column=1, sticky=tk.W)
+        self.system_selector = ttk.Combobox(system_frame, textvariable=self.console_url, values=consoles, width=50)
+        self.system_selector.grid(row=0, column=1, sticky=tk.W)
+
+        # Custom directory section
+        custom_dir_frame = ttk.LabelFrame(frame, text="Custom Download", padding="10")
+        custom_dir_frame.grid(row=1, column=0, columnspan=3, sticky=(tk.W, tk.E))
+
+        ttk.Checkbutton(custom_dir_frame, text="Advanced Mode", variable=self.advanced_mode, command=self.toggle_advanced).grid(row=1, column=0,                                                                                             sticky=tk.W)
+        self.url_entry_label = (tk.Label(custom_dir_frame, text="Custom URL:"))
+        self.url_entry_label.grid(row=2, column=0, sticky=tk.W)
+
+        self.url_entry_box = (ttk.Entry(custom_dir_frame, textvariable=self.custom_url, width=50))
+        self.url_entry_box.grid(row=2, column=1, sticky=tk.W)
+
+        self.toggle_advanced()
 
         # Region selection section
         region_frame = ttk.LabelFrame(frame, text="Region Selection", padding="10")
-        region_frame.grid(row=1, column=0, columnspan=3, sticky=(tk.W, tk.E))
+        region_frame.grid(row=2, column=0, columnspan=3, sticky=(tk.W, tk.E))
 
         ttk.Checkbutton(region_frame, text="Limit by region", variable=self.do_region_limit,
                         command=self.toggle_region_option).grid(row=0, column=0, sticky=tk.W)
+
         self.region_label = ttk.Label(region_frame, text="Region:", width=40)
         self.region_label.grid(row=1, column=0, sticky=tk.E)
         regions = ["Europe", "USA", "Japan", "China", "Korea", "Australia", "Asia", "World only"]
@@ -74,14 +95,17 @@ class DownloaderApp:
 
         # File limit section
         limit_frame = ttk.LabelFrame(frame, text="File Limit", padding="10")
-        limit_frame.grid(row=2, column=0, columnspan=3, sticky=(tk.W, tk.E))
+        limit_frame.grid(row=3, column=0, columnspan=3, sticky=(tk.W, tk.E))
 
         ttk.Checkbutton(limit_frame, text="Smart filters (Recommended)", variable=self.do_smart_limit).grid(
             row=0, column=0, sticky=tk.W)
 
+        """ttk.Checkbutton(limit_frame, text="Skip duplicates", variable=self.skip_duplicates).grid(
+            row=1, column=0, sticky=tk.W)"""
+
         # Output directory section
         output_frame = ttk.LabelFrame(frame, text="Output Directory", padding="10")
-        output_frame.grid(row=3, column=0, columnspan=3, sticky=(tk.W, tk.E))
+        output_frame.grid(row=4, column=0, columnspan=3, sticky=(tk.W, tk.E))
 
         ttk.Label(output_frame, text="Output dir:", width=20).grid(row=0, column=0, sticky=tk.W)
         ttk.Entry(output_frame, textvariable=self.output_dir, width=50).grid(row=0, column=1, sticky=tk.W)
@@ -89,14 +113,14 @@ class DownloaderApp:
 
         # Progress bar section
         progress_frame = ttk.LabelFrame(frame, text="Progress", padding="10")
-        progress_frame.grid(row=4, column=0, columnspan=3, sticky=(tk.W, tk.E))
+        progress_frame.grid(row=5, column=0, columnspan=3, sticky=(tk.W, tk.E))
 
         self.progress = ttk.Progressbar(progress_frame, orient="horizontal", length=400, mode="determinate")
         self.progress.grid(row=0, column=0, columnspan=3, pady=10, sticky=tk.W)
 
         # Download buttons section
         button_frame = ttk.LabelFrame(frame, text="Controls", padding="10")
-        button_frame.grid(row=5, column=0, columnspan=3, sticky=(tk.W, tk.E))
+        button_frame.grid(row=6, column=0, columnspan=3, sticky=(tk.W, tk.E))
 
         self.download_button = ttk.Button(button_frame, text="Start Download", command=self.start_download)
         self.download_button.grid(row=0, column=0, sticky=tk.W)
@@ -108,7 +132,7 @@ class DownloaderApp:
         self.pause_button.config(state=tk.DISABLED)
 
         # Status label
-        ttk.Label(frame, textvariable=self.status, width=80).grid(row=6, column=0, columnspan=3, pady=10, sticky=tk.W)
+        ttk.Label(frame, textvariable=self.status, width=80).grid(row=7, column=0, columnspan=3, pady=10, sticky=tk.W)
 
     def toggle_region_option(self):
         if self.do_region_limit.get():
@@ -118,11 +142,26 @@ class DownloaderApp:
             self.region_label.config(state=tk.DISABLED)
             self.region_selector.config(state=tk.DISABLED)
 
+    def toggle_advanced(self):
+        if self.advanced_mode.get():
+            self.system_selector.config(state=tk.DISABLED)
+            self.system_text.config(state=tk.DISABLED)
+            self.url_entry_label.config(state=tk.NORMAL)
+            self.url_entry_box.config(state=tk.NORMAL)
+        else:
+            self.system_selector.config(state=tk.NORMAL)
+            self.system_text.config(state=tk.NORMAL)
+            self.url_entry_label.config(state=tk.DISABLED)
+            self.url_entry_box.config(state=tk.DISABLED)
+
     def select_output_dir(self):
         self.output_dir.set(tk.filedialog.askdirectory())
 
     def start_download(self):
-        if not self.console_url.get():
+        if self.advanced_mode.get() and not self.console_url.get().startswith('https://myrient.erista.me/files/'):
+            messagebox.showerror("Unsupported link", "The custom link entered is not supported by MRScraper. Ensure that your link is part of Myrient's file directory.")
+            return
+        elif not self.console_url.get() and not self.advanced_mode.get():
             messagebox.showwarning("Warning", "Please select a console.")
             return
         elif not self.output_dir.get():
@@ -156,12 +195,20 @@ class DownloaderApp:
             self.pause_button.config(text="Pause Download")
 
     def download_files(self):
-        base_url = f"https://myrient.erista.me/files/No-Intro/{quote(self.console_url.get())}"
+        if not self.advanced_mode.get():
+            base_url = f"https://myrient.erista.me/files/No-Intro/{quote(self.console_url.get())}"
+        else:
+            base_url = self.custom_url.get()
 
         min_free_space = 100 * 1024 * 1024
 
-        response = requests.get(base_url)
-        response.raise_for_status()
+        try:
+            response = requests.get(base_url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            messagebox.showerror("Error", f"Failed to fetch URL: {e}")
+            self.curr_downloading = False
+            return
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -228,8 +275,8 @@ class DownloaderApp:
         self.cancel_button.config(state=tk.DISABLED)
         self.pause_button.config(state=tk.DISABLED)
 
-    def is_valid_file(self, file_url):
-        file_name = unquote(file_url.split('/')[-1])
+    def is_valid_file(self, href):
+        file_name = unquote(href.split('/')[-1])
         regions = [self.region.get(), "(World)"]
         excluded_keywords = [
             "(Beta)", "(Alpha)", "(Proto)",
